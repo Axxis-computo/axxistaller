becc785f9166a72a86bbb89df7c043fe710ede87
50c10 <cit_java@hotmail.com>
2014-01-21 00:40:26 -0600

    Login
    Cambio en la barra de login y logout mejorando un poco la vista y
    agregando una url para poder hacer logout sin nesesidad de utilizar un
    form

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('home.views',
   url(r'^$','home_view',name='Home_view'),
+  url(r'^logout/$','logout_view',name='logout_view'),
 )
